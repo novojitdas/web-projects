3cc36fb05b7e7951acd596a0e4d273e080878b03
novojitdas <novojitdas@gmail.com>
2023-08-26 23:41:30 +0600

    Todo-app: not started function defined. need work on in progress & completed section

---
+++ DOM/todo-app/todo-app.js
@@ -0,0 +1,29 @@
+/* Steps
+1. new-task button will add a task into not-startd
+2. 
+3.
+*/
+
+const newTaskButton = document.querySelector('#new-task');
+// Not Started
+// click event of newTaskButton
+newTaskButton.addEventListener('click',(event) =>{
+     const input = document.querySelector('#input-text');
+     const text = input.value;
+     //reseting input field
+     input.value = ''
+     // now creating input and label and adding text to label
+     const task = document.createElement('input');
+     task.type = 'checkbox';
+     const taskLabel = document.createElement('label');
+     taskLabel.textContent = text; 
+     // now appending into not started
+     const notStarted = document.querySelector('#not-started');
+     notStarted.appendChild(task);
+     notStarted.appendChild(taskLabel);
+     // adding new line afte every function call 
+     notStarted.appendChild(document.createElement('br'));
+
+});
+
+// test
